f75c5e53017a60af8aa6191bd54e02ffb4b156c9
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-11-16 08:41:04 +0100

    conversioneinsAscii

--- conversioneinsAscii/Program.cs
+++ conversioneinsAscii/Program.cs
@@ -6,7 +6,7 @@ namespace conversioneinsAscii
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ciao");
         }
     }
 }
