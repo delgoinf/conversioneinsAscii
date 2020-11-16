7bc4ef495419ef13557cfa36db6c5053e74786e5
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-11-16 08:39:31 +0100

    Aggiungere i file di progetto.

---
+++ conversioneinsAscii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace conversioneinsAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
